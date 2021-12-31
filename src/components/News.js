49a5b5366c4d567e41e698dbd5c5a1b874198bfe
Alex <alexbachurin91@gmail.com>
2021-12-31 08:50:23 +0300

    display news on page

--- src/components/News.js
+++ src/components/News.js
@@ -2,14 +2,30 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 import Loading from './Loading';
 const News = () => {
-    const { loading } = useGlobalContext();
+    const { loading, news } = useGlobalContext();
     if (loading) {
         return <Loading />
     }
     return (
-        <div>
-            News component
-        </div>
+        <section className='stories'>
+            {news.map(item => {
+                const { objectID, author, num_comments, points, title, url } = item;
+                return (
+                    <article key={objectID} className='story'>
+                        <h4 className="title">
+                            {title}
+                        </h4>
+                        <p className="info">
+                            {points} points by <span>{author} |</span> {num_comments} comments
+                        </p>
+                        <div>
+                            <a href={url} className='read-link' target='_blank' rel='noopener noreferrer'>read more</a>
+                            <button className='remove-btn'>remove</button>
+                        </div>
+                    </article>
+                )
+            })}
+        </section>
     )
 }
 
